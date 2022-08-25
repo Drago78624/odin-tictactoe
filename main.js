725518345a5597226d833343df51157d6fdeebbc
Drago78624 <maazahmed78624@gmail.com>
2022-08-25 18:58:52 +0500

    completed

--- main.js
+++ main.js
@@ -1,121 +1,119 @@
-import './style.css';
+import "./style.css";
 
-const boxes = document.querySelectorAll('.ttt-box');
-const box1 = document.querySelector('.ttt-box-1');
-const box2 = document.querySelector('.ttt-box-2');
-const box3 = document.querySelector('.ttt-box-3');
-const box4 = document.querySelector('.ttt-box-4');
-const box5 = document.querySelector('.ttt-box-5');
-const box6 = document.querySelector('.ttt-box-6');
-const box7 = document.querySelector('.ttt-box-7');
-const box8 = document.querySelector('.ttt-box-8');
-const box9 = document.querySelector('.ttt-box-9');
-const player = document.querySelector('.player');
-const overlay = document.querySelector('.overlay');
-const winningPlayer = document.querySelector('.winning-player');
-const playagainBtn = document.querySelector('.playagain-btn');
-const score1 = document.querySelector('.score1');
-const score2 = document.querySelector('.score2');
-const draw = document.querySelector('.draw');
-const win = document.querySelector('.win');
+const boxes = document.querySelectorAll(".ttt-box");
+const playagainBtn = document.querySelector(".playagain-btn");
+const score1 = document.querySelector(".score1");
+const score2 = document.querySelector(".score2");
 
-let sign = 'X';
 
-let p1Score = 0;
-let p2Score = 0;
-
-score1.innerText = JSON.parse(sessionStorage.getItem('player1Score'));
-score2.innerText = JSON.parse(sessionStorage.getItem('player2Score'));
-
-function setScore() {
-  sessionStorage.setItem('player1Score', JSON.stringify(p1Score));
-  sessionStorage.setItem('player2Score', JSON.stringify(p2Score));
-  score1.innerText = JSON.parse(sessionStorage.getItem('player1Score'));
-  score2.innerText = JSON.parse(sessionStorage.getItem('player2Score'));
+const Player = (name, marker)=>{
+  let score = 0
+  const getName = ()=> name
+  const getMarker = ()=> marker
+  const getScore = ()=> score
+  const increaseScore = () => score++
+  return { getName, getMarker, getScore, increaseScore }
 }
 
-playagainBtn.addEventListener('click', () => {
-  boxes.forEach((box) => {
-    box.innerText = '';
-    box.style.pointerEvents = 'auto';
-  });
-  overlay.classList.remove('flex');
-  overlay.classList.add('hidden');
-});
+const playerOne = Player("Player 1", "X")
+const playerTwo = Player("Player 2", "O")
+
+const GameBoard = (() => {
+  let gameboard = ["X", "", "", "", "", "", "", "", ""];
+  let sign = playerOne.getMarker();
+  return {gameboard, sign}
+})();
+
+score1.innerText = JSON.parse(sessionStorage.getItem("player1Score"));
+score2.innerText = JSON.parse(sessionStorage.getItem("player2Score"));
+
+const Controller = (()=>{
+  const player = document.querySelector(".player");
+  const overlay = document.querySelector(".overlay");
+  const winningPlayer = document.querySelector(".winning-player");
+  const draw = document.querySelector(".draw");
+  const win = document.querySelector(".win");
+
+  let {gameboard, sign} = GameBoard
+  const playAgain = () => {
+    boxes.forEach((box, i) => {
+      gameboard = ["", "", "", "", "", "", "", "", ""]
+      box.innerText = gameboard[i];
+      box.style.pointerEvents = "auto";
+    });
+    overlay.classList.remove("flex");
+    overlay.classList.add("hidden");
+  }
+  function makeMove() {
+    gameboard[parseInt(this.dataset.id)] = sign
+    console.log(gameboard);
+    this.innerText = gameboard[parseInt(this.dataset.id)];
+    this.style.pointerEvents = "none";
+    sign = sign == playerOne.getMarker() ? playerTwo.getMarker() : playerOne.getMarker();
+    onWin();
+    player.innerText = player.innerText == "1" ? "2" : "1";
+  }
+
+  const setScore = () => {
+    sessionStorage.setItem("player1Score", JSON.stringify(playerOne.getScore()));
+    sessionStorage.setItem("player2Score", JSON.stringify(playerTwo.getScore()));
+    score1.innerText = JSON.parse(sessionStorage.getItem("player1Score"));
+    score2.innerText = JSON.parse(sessionStorage.getItem("player2Score"));
+  }
+
+  function onWin() {
+    if (
+      (gameboard[0] === gameboard[1] && gameboard[1] === gameboard[2] && gameboard[0]) ||
+      (gameboard[3] === gameboard[4] && gameboard[4] === gameboard[5] && gameboard[3]) ||
+      (gameboard[6] === gameboard[7] && gameboard[7] === gameboard[8] && gameboard[6]) ||
+      (gameboard[0] === gameboard[3] && gameboard[3] === gameboard[6] && gameboard[0]) ||
+      (gameboard[1] === gameboard[4] && gameboard[4] === gameboard[7] && gameboard[1]) ||
+      (gameboard[2] === gameboard[5] && gameboard[5] === gameboard[8] && gameboard[2]) ||
+      (gameboard[0] === gameboard[4] && gameboard[4] === gameboard[8] && gameboard[0]) ||
+      (gameboard[2] === gameboard[4] && gameboard[4] === gameboard[6] && gameboard[2])
+    ) {
+      winningPlayer.innerText = player.innerText;
+      win.classList.remove("hidden");
+      win.classList.add("block");
+      draw.classList.add("hidden");
+      overlay.classList.remove("hidden");
+      overlay.classList.add("flex");
+      // console.log(player.innerText);
+      // console.log(winningPlayer.innerText);
+      if (winningPlayer.innerText == "1") {
+        playerOne.increaseScore();
+        setScore();
+      } else if (winningPlayer.innerText == "2") {
+        playerTwo.increaseScore();
+        setScore();
+      }
+      console.log(`${player.innerText} wins`);
+    } else if (
+      gameboard[0] != "" &&
+      gameboard[1] != "" &&
+      gameboard[2] != "" &&
+      gameboard[3] != "" &&
+      gameboard[4] != "" &&
+      gameboard[5] != "" &&
+      gameboard[6] != "" &&
+      gameboard[7] != "" &&
+      gameboard[8] != ""
+    ) {
+      console.log("draw");
+      win.classList.add("hidden");
+      draw.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+      overlay.classList.add("flex");
+    }
+  }
+
+  return { playAgain, makeMove }
+})()
+
+playagainBtn.addEventListener("click", Controller.playAgain);
 
 boxes.forEach((box) => {
-  box.addEventListener('click', onCheck);
+  box.addEventListener("click", Controller.makeMove);
 });
 
-function onCheck() {
-  this.innerText = sign;
-  this.style.pointerEvents = 'none';
-  sign = sign == 'X' ? 'O' : 'X';
-  onWin();
-  player.innerText = player.innerText == '1' ? '2' : '1';
-}
 
-function onWin() {
-  console.log('onWin function started');
-  if (
-    (box1.innerText == 'X' && box2.innerText == 'X' && box3.innerText == 'X') ||
-    (box1.innerText == 'O' && box2.innerText == 'O' && box3.innerText == 'O') ||
-    (box1.innerText == 'X' && box5.innerText == 'X' && box9.innerText == 'X') ||
-    (box1.innerText == 'O' && box5.innerText == 'O' && box9.innerText == 'O') ||
-    (box1.innerText == 'X' && box4.innerText == 'X' && box7.innerText == 'X') ||
-    (box1.innerText == 'O' && box4.innerText == 'O' && box7.innerText == 'O') ||
-    (box2.innerText == 'X' && box5.innerText == 'X' && box8.innerText == 'X') ||
-    (box2.innerText == 'O' && box5.innerText == 'O' && box8.innerText == 'O') ||
-    (box2.innerText == 'X' && box5.innerText == 'X' && box8.innerText == 'X') ||
-    (box2.innerText == 'O' && box5.innerText == 'O' && box8.innerText == 'O') ||
-    (box3.innerText == 'X' && box6.innerText == 'X' && box9.innerText == 'X') ||
-    (box3.innerText == 'O' && box6.innerText == 'O' && box9.innerText == 'O') ||
-    (box3.innerText == 'X' && box6.innerText == 'X' && box9.innerText == 'X') ||
-    (box3.innerText == 'O' && box6.innerText == 'O' && box9.innerText == 'O') ||
-    (box3.innerText == 'X' && box5.innerText == 'X' && box7.innerText == 'X') ||
-    (box3.innerText == 'O' && box5.innerText == 'O' && box7.innerText == 'O') ||
-    (box3.innerText == 'X' && box5.innerText == 'X' && box7.innerText == 'X') ||
-    (box3.innerText == 'O' && box5.innerText == 'O' && box7.innerText == 'O') ||
-    (box4.innerText == 'X' && box5.innerText == 'X' && box6.innerText == 'X') ||
-    (box4.innerText == 'O' && box5.innerText == 'O' && box6.innerText == 'O') ||
-    (box4.innerText == 'X' && box5.innerText == 'X' && box6.innerText == 'X') ||
-    (box4.innerText == 'O' && box5.innerText == 'O' && box6.innerText == 'O') ||
-    (box7.innerText == 'X' && box8.innerText == 'X' && box9.innerText == 'X') ||
-    (box7.innerText == 'O' && box8.innerText == 'O' && box9.innerText == 'O') ||
-    (box7.innerText == 'X' && box8.innerText == 'X' && box9.innerText == 'X') ||
-    (box7.innerText == 'O' && box8.innerText == 'O' && box9.innerText == 'O')
-  ) {
-    winningPlayer.innerText = player.innerText;
-    win.classList.remove('hidden');
-    win.classList.add('block');
-    draw.classList.add('hidden');
-    overlay.classList.remove('hidden');
-    overlay.classList.add('flex');
-    console.log(player.innerText);
-    console.log(winningPlayer.innerText);
-    if (winningPlayer.innerText == '1') {
-      p1Score++;
-      setScore();
-    } else if (winningPlayer.innerText == '2') {
-      p2Score++;
-      setScore();
-    }
-    console.log(`${player.innerText} wins`);
-  } else if (
-    box1.innerText != '' &&
-    box2.innerText != '' &&
-    box3.innerText != '' &&
-    box4.innerText != '' &&
-    box5.innerText != '' &&
-    box6.innerText != '' &&
-    box7.innerText != '' &&
-    box8.innerText != '' &&
-    box9.innerText != ''
-  ) {
-    console.log('draw');
-    win.classList.add('hidden');
-    draw.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    overlay.classList.add('flex');
-  }
-}
